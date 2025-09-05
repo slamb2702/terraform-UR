8c6c17e050130e075612f5643f38b6825db29d78
slamb2702 <lambstephen26@gmail.com>
2025-09-04 17:10:21 -0700

    copied version

---
+++ stage/services/hello-world-app/data.tf
@@ -0,0 +1,25 @@
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners      = ["099720109477"] # Canonical
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+# }
+
+
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami*"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
